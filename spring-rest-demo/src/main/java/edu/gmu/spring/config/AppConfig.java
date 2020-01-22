bb42c5eacb1866987717fb7ee25b613c2ddc9c37
Changhoon Ji <cji2@gmu.edu>
2020-01-22 13:05:00 -0500

    create AppConfig.java for @Configuration, @EnableWebMvc, etc.

---
+++ spring-rest-demo/src/main/java/edu/gmu/spring/config/AppConfig.java
@@ -0,0 +1,12 @@
+package edu.gmu.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="edu.gmu.spring")
+public class AppConfig {
+
+}
