88046e084e8084f2daa338971c992da85c5a4758
Changhoon Ji <cji2@gmu.edu>
2020-01-23 12:45:08 -0500

    modify Rest Controller to load students only once, using @PostConstruct

--- spring-rest-demo/src/main/java/edu/gmu/spring/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/edu/gmu/spring/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package edu.gmu.spring.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +16,40 @@ import edu.gmu.spring.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// define end-point for "/students" - return list of students.
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
+	private List<Student> students;
+	
+	// define @PostConstruct to load the student .. only once!
+	@PostConstruct
+	public void loadData() {
+		
+		students = new ArrayList<>();
 		
 		// create some sample students.
 		// it will be replaced with database later.
 		students.add(new Student("Abraham", "Lee"));
 		students.add(new Student("David", "Ji"));
 		students.add(new Student("Jacob", "Kim"));
+	}
+	
+	// define end-point for "/students" - return list of students.
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		
 		return students;
 	}
+	
+		// define end-point for "/students/{stduentId}" - return student at index.
+	/*
+		@GetMapping("/students/{studentId}")
+		public List<Student> getStudent(@PathVariable int studentId) {
+			List<Student> students = new ArrayList<>();
+			
+			// create some sample students.
+			// it will be replaced with database later.
+			students.add(new Student("Abraham", "Lee"));
+			students.add(new Student("David", "Ji"));
+			students.add(new Student("Jacob", "Kim"));
+			
+			return students.get(studentId);
+		} */
 }
